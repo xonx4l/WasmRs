3373b510908ffbef4878345f914a2f79d4b2fb91
xonx4l <sk797119@gmail.com>
2025-07-15 17:44:37 +0530

    add fn live_neighbour_count

--- src/lib.rs
+++ src/lib.rs
@@ -20,4 +20,20 @@ impl universe {
     fn get_index(&self , width: u32,height: u32) -> usize {
       (row * self.width + column) as usize 
     }
+
+    fn live_neighbour_count(&self , row: u32 , column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1,0,1].iter().cloned() {
+            for delta_col in [self.width - 1,0,1].iter.cloned(){
+                if delta_row == 0 && delta_col ==0 {
+                    continue;
+                }
+                let neighbour_row = (row + delta_row) % self.height;
+                let neighbour_column = (column + delta_col) % self.width;
+                let idx = self.get.index(neighbour_row, neighbour_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
 }
