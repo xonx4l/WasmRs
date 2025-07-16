b4b20c2eba080d456ec44021d4f258a158103345
xonx4l <sk797119@gmail.com>
2025-07-16 23:25:19 +0530

    fix bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,37 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
 use std::fmt;
 
 #[wasm_bindgen]
 #[repr(u8)]
-#[derive(Clone, Copy, Debug , PartialEq, Eq)]
-pub enum cell {
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
     Dead = 0,
     Alive = 1,
 }
 
-pub struct universe {
-    height: u32,
+#[wasm_bindgen]
+pub struct Universe {
     width: u32,
+    height: u32,
     cells: Vec<Cell>,
 }
 
 #[wasm_bindgen]
-impl universe {
-    fn get_index(&self , width: u32,height: u32) -> usize {
-      (row * self.width + column) as usize 
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
     }
 
-    fn live_neighbour_count(&self , row: u32 , column: u32) -> u8 {
+    fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
-        for delta_row in [self.height - 1,0,1].iter().cloned() {
-            for delta_col in [self.width - 1,0,1].iter.cloned(){
-                if delta_row == 0 && delta_col ==0 {
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
                     continue;
                 }
                 let neighbour_row = (row + delta_row) % self.height;
                 let neighbour_column = (column + delta_col) % self.width;
-                let idx = self.get.index(neighbour_row, neighbour_col);
+                let idx = self.get_index(neighbour_row, neighbour_column);
                 count += self.cells[idx] as u8;
             }
         }
@@ -44,19 +43,19 @@ impl universe {
 
         for row in 0..self.height {
             for col in 0..self.width {
-            let idx = self.get_index(row, col);
-            let cell = self.cells[idx];
-            let live_neighbours = self.live_neighbours_count(row, col);
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbours = self.live_neighbour_count(row, col);
 
-            let next cell = match (cell , live_neighbours) {
-                (Cell::Alive, x) if x < 2 => Cell::Dead,
-                (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                (Cell::Alive, x) if x > 3 => Cell::Dead,
-                (Cell::Dead, 3) => Cell::Alive,
-                (otherwise, _) => otherwise,
-            };
+                let next_cell = match (cell, live_neighbours) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise,
+                };
 
-            next[idx] = next_cell;
+                next[idx] = next_cell;
             }
         }
 
@@ -64,24 +63,25 @@ impl universe {
     }
 
     pub fn new() -> Universe {
+
         let width = 64;
         let height = 64;
 
         let cells = (0..width * height)
-          .map(|i| {
-              if i % 2 == 0 || i % 7 == 0 {
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
                     Cell::Alive
-              } else {
+                } else {
                     Cell::Dead
-              }
-          })
-          .collect();
+                }
+            })
+            .collect();
 
-          Universe {
-              width,
-              height,
-              cells,
-          }
+        Universe {
+            width,
+            height,
+            cells,
+        }
     }
 
     pub fn render(&self) -> String {
@@ -89,17 +89,16 @@ impl universe {
     }
 }
 
-impl fmt::Display for universe {
-    fn fmt (&self, f: &mut fmt::Formatter) ->  fmt.Result {
-        for line in self.cells.as_slice().chunks(self width as usize){
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
             for &cell in line {
                 let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
                 write!(f, "{}", symbol)?;
             }
-        write!(f, "\n")?;
-    }
-    
-    Ok(())
-  }
-}
+            write!(f, "\n")?;
+        }
 
+        Ok(())
+    }
+}
