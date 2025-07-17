bdea5e62b4a0da87b796c857e70955413080e00b
xonx4l <sk797119@gmail.com>
2025-07-17 21:13:52 +0530

    add changes

--- tests/web.rs
+++ tests/web.rs
@@ -1,9 +1,8 @@
-//! Test suite for the Web and headless browsers.
 
 #![cfg(target_arch = "wasm32")]
 
-extern crate wasm_bindgen_test;
-use wasm_bindgen_test::*;
+extern crate wasm_game_of_life;
+use wasm_game_of_life::Universe;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
